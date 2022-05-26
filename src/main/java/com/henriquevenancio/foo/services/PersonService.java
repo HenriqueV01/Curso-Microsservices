94e3d70fa4823b30808e2a9db0ee0ce9cf7493a3
Henrique <henrique.unibratec01@gmail.com>
2022-05-26 15:01:33 -0300

    PersonController PersonService POST PUT DELETE

--- src/main/java/com/henriquevenancio/foo/services/PersonService.java
+++ src/main/java/com/henriquevenancio/foo/services/PersonService.java
@@ -1,5 +1,7 @@
 package com.henriquevenancio.foo.services;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.stereotype.Service;
@@ -19,8 +21,52 @@ public class PersonService {
 		person.setAddress("Recife - PE");
 		person.setGender("Masculino");		
 		
-		return person ;
+		return person;
 		
 	}
+	
+	public List<Person> findAll() {		
+		List<Person> persons = new ArrayList<Person>();
+		
+		for (int i = 0; i < 10; i++) {
+			Person person = mockPerson(i);			
+			persons.add(person);
+		}		
+		return persons;		
+	}
+
+	private Person mockPerson(int i) {
+		Person person =  new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Henrique" + i);
+		person.setLasttName("Venancio" + i);
+		person.setAddress("Recife - PE");
+		person.setGender("Masculino");		
+		
+		return person;
+	}
+	
+	public Person create(Person person) {
+		return person;
+	}
+	
+	public Person update(Person person) {
+		return person;
+	}
+	
+	public void delete(String id) {
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
